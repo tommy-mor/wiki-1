7ab9cf57b61a5186c7247a6c311fb717295b8c20
Jacob Chvatal <jake@isnt.online>
2021-02-17 12:09:10 -0800

    Remove haskell work

--- default.nix
+++ default.nix
@@ -3,12 +3,14 @@
 let
   # https://stackoverflow.com/questions/54810851/how-can-i-build-a-haskell-dependency-from-a-github-source-nix-file-using-nix
   hs = pkgs.haskellPackages.extend (self: super: { # (1) extend the package set
-  my-orgmode-parse = self.callPackage ./orgmode-parse.nix {};
-      # orgmode-parse.defaultPackage."x86_64-linux".lib; # pkgs.haskell.lib.dontCheck (self.callPackage .orgmode-parse { }); # use my local orgmode-parse
+    my-orgmode-parse = orgmode-parse {
+      inherit pkgs;
+    }; # self.callPackage ./orgmode-parse.nix {};
+    # orgmode-parse.defaultPackage."x86_64-linux".lib; # pkgs.haskell.lib.dontCheck (self.callPackage .orgmode-parse { }); # use my local orgmode-parse
   });
 
   haskellDeps = ps:
-    with ps; [
+    with hs; [
       base
       lens
       mtl
@@ -19,7 +21,7 @@ let
       turtle
       attoparsec
       brittany
-      hs.my-orgmode-parse
+      my-orgmode-parse
       universum
       clay
       lucid
